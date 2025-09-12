7b4e13031b34c44add5dcc9670b0176c24f2a0af
Daria.Moreno-Gogoleva <daria.moreno-gogoleva@jetbrains.com>
2025-09-12 12:40:03 +0200

    JS-23262 fix

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -564,9 +564,7 @@ class Widget extends Component {
     const { socket, customData } = this.props;
     const sessionId = this.getSessionId();
     this.props.dispatch(clearMessages());
-    console.log('restart', socket, this.props, sessionId, customData);
     socket.emit('user_uttered', { message: '/restart', customData, session_id: sessionId });
-    socket.close();
   }
 
   render() {
